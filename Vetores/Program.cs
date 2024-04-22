347b626e7c6c1febc1f6efe52b73d752c1635012
pestana1984 <pestana1984@gmail.com>
2024-04-22 13:40:06 -0300

    Add project files.

---
+++ Vetores/Program.cs
@@ -0,0 +1,15 @@
+﻿int numero;
+int[] numeros = new int[5];
+
+
+for (int i = 0; i < 5; i++)
+{
+    numero = int.Parse(Console.ReadLine());
+    numeros[i] = numero;
+    //numeros[i] = int.Parse(Console.ReadLine());
+}
+
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine("valor: " + numeros[i]);
+}
